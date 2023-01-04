1b01ce8ac864d9ba9cf0da1f58ba876464892d3b
Harsh70830 <harshsharma70830@gmail.com>
2023-01-04 18:47:33 +0530

    initial commit

---
+++ client/src/dummyData.js
@@ -0,0 +1,53 @@
+export const Users = [
+  {
+    id:1,
+    profilePicture: "person/1.jpeg",
+    username: "Virat Kohli",
+  },
+  {
+    id:2,
+    profilePicture: "person/2.jpeg",
+    username: "MS Dhoni",
+  },
+  {
+    id:3,
+    profilePicture: "person/3.jpeg",
+    username: "Hardik Pandya",
+  },
+  {
+    id:4,
+    profilePicture: "person/4.jpeg",
+    username: "KL Rahul",
+  },
+  {
+    id:5,
+    profilePicture: "person/5.jpeg",
+    username: "Rishabh Pant",
+  },
+  {
+    id:6,
+    profilePicture: "person/6.jpeg",
+    username: "Suryakumar Yadav",
+  },
+  {
+    id:7,
+    profilePicture: "person/7.jpeg",
+    username: "Ravindra Jadeja",
+  },
+  {
+    id:8,
+    profilePicture: "person/8.jpeg",
+    username: "Shikhar Dhawan",
+  },
+  {
+    id:9,
+    profilePicture: "person/9.jpeg",
+    username: "Yuzvendra Chahal",
+  },
+  {
+    id:10,
+    profilePicture: "person/10.jpeg",
+    username: "Shreyas Iyer"
+  },
+];
+
